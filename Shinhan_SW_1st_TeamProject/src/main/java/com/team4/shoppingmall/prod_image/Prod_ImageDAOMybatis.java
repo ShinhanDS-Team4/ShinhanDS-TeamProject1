5dd7f789a70d5ddd6fc2e07260075cb3aeef6f6a
1nthebleakmidwinter <1nth2bleakmidwinter@gmail.com>
2024-06-17 11:31:27 +0900

    prod and prod_image work

---
+++ Shinhan_SW_1st_TeamProject/src/main/java/com/team4/shoppingmall/prod_image/Prod_ImageDAOMybatis.java
@@ -0,0 +1,46 @@
+package com.team4.shoppingmall.prod_image;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Prod_ImageDAOMybatis implements Prod_ImageDAOInterface {
+
+	@Autowired
+	SqlSession sqlSession;
+	
+	String namespace = "com.saren.prod_image.";
+	
+	@Override
+	public Prod_ImageDTO selectByImageId(Integer img_id) {
+		return sqlSession.selectOne(namespace+"selectByImageId", img_id);
+	}
+
+	@Override
+	public Prod_ImageDTO selectByProdId(Integer prod_id) {
+		return sqlSession.selectOne(namespace+"selectByProdId", prod_id);
+	}
+
+	@Override
+	public List<Prod_ImageDTO> selectAll() {
+		return sqlSession.selectList(namespace+"selectAll");
+	}
+
+	@Override
+	public int prod_imageInsert(Prod_ImageDTO prod_image) {
+		return sqlSession.insert(namespace+"prod_imageInsert", prod_image);
+	}
+
+	@Override
+	public int prod_imageUpdate(Prod_ImageDTO prod_image) {
+		return sqlSession.update(namespace+"prod_imageUpdate", prod_image);
+	}
+
+	@Override
+	public int prod_imageDelete(Integer img_id) {
+		return sqlSession.delete(namespace+"prod_imageDelete", img_id);
+	}
+}
