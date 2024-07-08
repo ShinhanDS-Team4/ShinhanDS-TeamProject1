fc1ac87056a613de0c2b6d0dfbb5da6735e76692
sangho5550 <baeksh0118@gmail.com>
2024-07-09 04:25:27 +0900

    수정해야함

--- Shinhan_SW_1st_TeamProject/src/main/java/com/team4/shoppingmall/wish_list/Wish_ListService.java
+++ Shinhan_SW_1st_TeamProject/src/main/java/com/team4/shoppingmall/wish_list/Wish_ListService.java
@@ -2,6 +2,8 @@ package com.team4.shoppingmall.wish_list;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,11 +23,7 @@ public class Wish_ListService {
 	
 	public List<Wish_ListDTO> selectByProdId(Integer prod_id) {
 		return wish_listDAO.selectByProdId(prod_id);
-	}
-	
-	public List<Wish_ListDTO> selectAll(String memberId) {
-		return wish_listDAO.selectAll(memberId);
-	}
+	}	
 	
 	public int wish_listInsert(Wish_ListDTO wish_list) {
 		return wish_listDAO.wish_listInsert(wish_list);
@@ -35,11 +33,26 @@ public class Wish_ListService {
 		return wish_listDAO.wish_listUpdate(wish_list);
 	}
 	
-	public int wish_listDelete1(String member_id, Integer prod_id) {
-		return wish_listDAO.wish_listDelete1(member_id, prod_id);
-	}
+//	public int wish_listDelete1(String member_id, Integer prod_id) {
+//		return wish_listDAO.wish_listDelete1(member_id, prod_id);
+//	}
 
 	public int wish_listDelete(Wish_ListDTO wishDelete) {
 		return wish_listDAO.wish_listDelete(wishDelete);		
+	}
+
+	public List<Wish_ListDTO> selectAll(String memberId) {
+		return wish_listDAO.selectAll(memberId);
+	}
+	
+	public int selectWish(String customerId, String prod_id) {
+		return wish_listDAO.selectWish(customerId, prod_id);
 	}	
+	
+	public int selectDelete(String customerId, String prod_id) {
+		return wish_listDAO.selectDelete(customerId, prod_id);
+	}
+	public int wishAllDelete(String customerId) {
+		return wish_listDAO.wishAllDelete(customerId);
+	}
 }
